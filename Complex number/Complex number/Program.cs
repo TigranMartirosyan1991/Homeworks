4ff758a64b09438d3d672e531e77828911ee3ddc
Tigran# <Tigran#@LAPTOP-0CV90VBK>
2019-03-27 01:19:27 +0400

    class division

--- Complex number/Complex number/Program.cs
+++ Complex number/Complex number/Program.cs
@@ -38,30 +38,30 @@ namespace Complex_number
         public static Complex Division(Complex a, Complex b)
         {
             Complex result = new Complex();
-            result.r = a.r * b.r + a.i * b.i / b.r * b.r + b.i * b.i;
-            result.i = a.i * b.r - a.r * b.i / b.r * b.r + b.i * b.i;
+            result.r = (a.r * b.r + a.i * b.i) /(Math.Pow(b.r,2.0) + Math.Pow(b.i,2.0));
+            result.i = (a.i * b.r - a.r * b.i) /(Math.Pow(b.r,2.0) +Math.Pow(b.i,2.0));
             return result;
         }
-     
+
         public static Complex operator +(Complex a, Complex b)
         {
             return Complex.Sum(a, b);
         }
-        public static Complex operator * (Complex a,Complex b)
+        public static Complex operator *(Complex a, Complex b)
         {
-            return Complex.Multiplication(a,b);
+            return Complex.Multiplication(a, b);
         }
-        public static Complex  operator / (Complex a,Complex b)
+        public static Complex operator /(Complex a, Complex b)
         {
             return Complex.Division(a, b);
         }
-        public static  Complex operator - (Complex a,Complex b)
+        public static Complex operator -(Complex a, Complex b)
         {
             return Complex.Subtract(a, b);
         }
         public override string ToString()
         {
-            return String.Format("{0} + i{1}", this.r, this.i);
+            return String.Format("{0} + {1}i", this.r, this.i);
         }
 
     }
@@ -75,7 +75,7 @@ namespace Complex_number
             c1.i = 3;
             c2.r = 7;
             c2.i = 9;
-            Complex Sum = c1 + c2;
+            Complex Sum = c1 / c2;
             Console.WriteLine(" new complex number is :{0}", Sum);
             Console.ReadKey();
 
