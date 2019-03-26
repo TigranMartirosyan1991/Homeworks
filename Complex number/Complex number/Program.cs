128bb517331e3366313e00d0b0f4cabc72e7247a
Tigran# <Tigran#@LAPTOP-0CV90VBK>
2019-03-26 23:37:04 +0400

    adding console

--- Complex number/Complex number/Program.cs
+++ Complex number/Complex number/Program.cs
@@ -9,33 +9,59 @@ namespace Complex_number
     class Complex
     {
         public double r, i;
-        public static Complex Sum(Complex a,Complex b)
+        public Complex()
+        {
+            this.r = 0.0;
+            this.i = 0.0;
+        }
+        public static Complex Sum(Complex a, Complex b)
         {
             Complex result = new Complex();
             result.r = a.r + b.r;
-            result. i = a.i + b.i;
+            result.i = a.i + b.i;
             return result;
         }
-        public static  Complex Multiplication (Complex a,Complex b)
+        public static Complex Multiplication(Complex a, Complex b)
         {
             Complex result = new Complex();
-            result.r = a.r * b.r- a.i * b.i;
-            result.i = a.i * b.r + a.r *b.i;
+            result.r = a.r * b.r - a.i * b.i;
+            result.i = a.i * b.r + a.r * b.i;
             return result;
         }
-        public static Complex Subtract ( Complex a,Complex b)
+        public static Complex Subtract(Complex a, Complex b)
         {
             Complex result = new Complex();
             result.r = a.r - b.r;
             result.i = a.i - b.i;
             return result;
         }
-        public  static Complex Division ( Complex a,Complex b)
+        public static Complex Division(Complex a, Complex b)
         {
             Complex result = new Complex();
             result.r = a.r * b.r + a.i * b.i / b.r * b.r + b.i * b.i;
             result.i = a.i * b.r - a.r * b.i / b.r * b.r + b.i * b.i;
             return result;
+        }
+     
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return Complex.Sum(a, b);
+        }
+        public static Complex operator * (Complex a,Complex b)
+        {
+            return Complex.Multiplication(a,b);
+        }
+        public static Complex  operator / (Complex a,Complex b)
+        {
+            return Complex.Division(a, b);
+        }
+        public static  Complex operator - (Complex a,Complex b)
+        {
+            return Complex.Subtract(a, b);
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} + i{1}", this.r, this.i);
         }
 
     }
@@ -43,6 +69,17 @@ namespace Complex_number
     {
         static void Main(string[] args)
         {
+            Complex c1 = new Complex();
+            Complex c2 = new Complex();
+            c1.r = 5;
+            c1.i = 3;
+            c2.r = 7;
+            c2.i = 9;
+            Complex Sum = c1 + c2;
+            Console.WriteLine(" new complex number is :{0}", Sum);
+            Console.ReadKey();
+
+
         }
     }
 }
